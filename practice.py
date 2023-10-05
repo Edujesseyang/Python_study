3860ef13984f2872088dca35873c5a2060d3d519
Edujesseyang <edujesseyang@gmail.com>
2023-10-04 20:41:22 -0700

    20231004

--- practice.py
+++ practice.py
@@ -1,45 +1,54 @@
-import math
-import random
+# import math
+# import random
 
-# -----------------problem 1-----------------
+# # -----------------problem 1-----------------
 
-year_wage = int(input("Enter yearly wage:"))
+# year_wage = int(input("Enter yearly wage:"))
 
-weekly_wage = year_wage / 52
-print("The weekly wage is :", weekly_wage)
+# weekly_wage = year_wage / 52
+# print("The weekly wage is :", weekly_wage)
 
-daily_wage = weekly_wage / 5
-print("The daily_wage is :", daily_wage)
+# daily_wage = weekly_wage / 5
+# print("The daily_wage is :", daily_wage)
 
-hourly_wage = daily_wage / 8
-print("The hourly_wage is:", hourly_wage)
+# hourly_wage = daily_wage / 8
+# print("The hourly_wage is:", hourly_wage)
 
-# -----------------problem 2-----------------
+# # -----------------problem 2-----------------
 
-weekly_wage = math.ceil(year_wage / 52)
-print("The round off weekly wage is:", weekly_wage)
+# weekly_wage = math.ceil(year_wage / 52)
+# print("The round off weekly wage is:", weekly_wage)
 
-daily_wage = math.ceil(weekly_wage / 5)
-print("The round off daily wage is:", daily_wage)
+# daily_wage = math.ceil(weekly_wage / 5)
+# print("The round off daily wage is:", daily_wage)
 
-hourly_wage = math.ceil(daily_wage / 8)
-print("The round off hourly wage is:", hourly_wage)
+# hourly_wage = math.ceil(daily_wage / 8)
+# print("The round off hourly wage is:", hourly_wage)
 
-# -----------------problem 3-----------------
+# # -----------------problem 3-----------------
 
-side1 = int(input("Enter the first side length:"))
-side2 = int(input("Enter the second side length:"))
+# side1 = int(input("Enter the first side length:"))
+# side2 = int(input("Enter the second side length:"))
 
-hypotenuse = math.sqrt(side1 ** 2 + side2 ** 2)
+# hypotenuse = math.sqrt(side1 ** 2 + side2 ** 2)
 
-print("The hypotenuse of the triangle is:", f'{hypotenuse:.0f}')
+# print("The hypotenuse of the triangle is:", f'{hypotenuse:.0f}')
 
-# -----------------problem 4------------------
+# # -----------------problem 4------------------
 
-date = int(input("Enter a date in format of MMDDYYYY:"))
-month = date // 1000000
-year = date % 10000
-day = date // 10000 % 100
-print("The month is:", month)
-print("The year is:", year)
-print("The day is:", day)
+# date = int(input("Enter a date in format of MMDDYYYY:"))
+# month = date // 1000000
+# year = date % 10000
+# day = date // 10000 % 100
+# print("The month is:", month)
+# print("The year is:", year)
+# print("The day is:", day)
+
+# -------------------try out----------------------
+input_num = float(input("Enter a float number:"))
+
+print("Round off the number:" ,round(input_num))
+
+roundOf_2 = round(input_num, 2)
+
+print("Round it off 2 decimal:", roundOf_2)
