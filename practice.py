066e551405eb596827187e6de0d69bba1ef5e2c1
Edujesseyang <edujesseyang@gmail.com>
2023-10-11 18:50:27 -0700

    20231011

--- practice.py
+++ practice.py
@@ -1,5 +1,6 @@
 import math
 import random
+from collections import namedtuple
 
 # # -----------------problem 1-----------------
 
@@ -61,3 +62,85 @@ import random
 # roundOf_2 = round(input_num, 2)
 
 # print("Round it off 2 decimal:", roundOf_2)
+
+# -------This is tuple-------------
+# sth = (1, 2, 3, 4, 5)
+# x = len(sth) + sth[0]
+
+# print("len + first is:", x )
+# print(sth)
+
+# ----------This is namedtuple------------
+# my_class = namedtuple('my_cass', ['first_class', 'second_class', 'third_class'])
+
+# today_class = my_class('ENG10', 'Math1A', 'CIS40')
+
+# print(today_class)
+
+# --------------------------------------------
+
+# score = {"math": 100, 'english':53, 'eng10':34}
+# print(score['math'])
+# if score['math'] != 100:
+#     print("Stupid")
+# else:
+#     print("good job")
+
+# -------------------------EX 1 cpt 3--------------------
+# name = str(input("Enter a name: "))
+
+# len = len(name)
+
+# if len % 3 == 0 :
+#     print("city name: ", name)
+# else:
+#     print(name[0], name[-1])
+
+# print("The length is :", len)
+
+
+# ---------------------EX 2---------------------------
+# names0 = str(input("Enter 1 name: "))
+# names1 = str(input("Enter 2 name: "))
+# names2 = str(input("Enter 3 name: "))
+# names3 = str(input("Enter 4 name: "))
+# names4 = str(input("Enter 5 name: "))
+# names = [names0, names1, names2, names3, names4]
+
+# print(names)
+# tem_name = names0
+# if names0 < names1:
+#     tem_name = names0
+# if names1 < names2:
+#     tem_name= names3
+# print(tem_name)  = names1
+# if names2 < names3:
+#     tem_name = names2
+# if names3 < names4:
+#     tem_name 
+
+# ---------------------EX 3---------------
+# temp = {}
+# temp['San F'] = 50
+# temp["LA"] = 54
+# temp['nyc'] = 34
+
+# print(temp)
+# del temp['LA']
+# print(temp)
+
+# temp['nyc'] = 54
+# print(temp)
+
+#____________________________EX4____________
+
+age = int(input("Enter your age: "))
+ht = int(input("Enter your height in inches: "))
+
+if age < 18:
+    if ht < 50:
+        print("You cannot ride")
+    else:
+        print("you can ride with parents")
+else:
+    print("you can ride")
