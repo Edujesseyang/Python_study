1dd786f2cb23604205022699d7adbcd2184ffee0
Edujesseyang <edujesseyang@gmail.com>
2023-10-17 15:51:56 -0700

    a

--- practice.py
+++ practice.py
@@ -255,3 +255,18 @@ from collections import namedtuple
 # print("跟你聊天真开心, 再见!")
 
 #----------------不是能智能, 哈哈---------------
+
+# -----input nums to get the avg---------
+# usage: keep input nums, when finish, input 0. 
+
+ipt_current = int(input("Enter your numbers: "))
+sum_nums = 0
+ipt_count = 0
+
+while  ipt_current != 0:
+    sum_nums = sum_nums + ipt_current
+    ipt_count += 1
+    ipt_current = int(input("Keep entering, enter 0 to start calculating: "))
+print("The average of those numbers is:", sum_nums / ipt_count)
+
+#---------输入数字 最后output这些数字的平均值.---------
