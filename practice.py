5f51ebbe682528711ea23d069178198a80a40071
Edujesseyang <edujesseyang@gmail.com>
2023-10-17 22:05:22 -0700

    sth

--- practice.py
+++ practice.py
@@ -331,5 +331,31 @@ from collections import namedtuple
 
 # ----------------above not work well, but close--------
 
+# ---------- calculate Resistance of a circuit---------
 
+# Rt = 0
+# ipt = 1
+# while ipt != 0:
+    
+#     print("Enter 0 to start calculate!")
+#     R = float(input("Enter the resistance R: "))
+#     ipt = R
+#     bool = input("Is is series or parallel, enter 's' / 'p': ")
+#     if bool == "s":
+#         Rt = Rt + R
+#     else:
+#         R = R + (1 / R) 
+#         Rt = 1 / R
+    
+
+# print(f'The total resistance of the circuit is {Rt}')
+#-------------above is not working good-------------
+
+lst = [1,2,3,4,5,6]
+
+for num in reversed(lst):
+    print(f'The num in reversed order is {num}')
+    
+    
+# This is the reversed check
 
