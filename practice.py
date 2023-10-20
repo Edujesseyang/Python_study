30171eda713aaebd82f2e30fef49e48b1c8adcd9
Edujesseyang <edujesseyang@gmail.com>
2023-10-19 19:18:16 -0700

    1

--- practice.py
+++ practice.py
@@ -474,3 +474,35 @@ from collections import namedtuple
 # for (index, value) in enumerate(lst):
 #     print(f'{index} : {lst[index]}')
 # ---------用 enumerate() 方程来实现 显示 index 和 value ----------
+
+# ----------------trig calculator---------------
+# a = float(input("Enter a side: "))
+# b = float(input("Enter b side: "))
+# c = float(input("Enter c side: "))
+# cos_A = ((c ** 2) + (b ** 2) - (a ** 2)) / (2 * b * c)
+# cos_B = ((c ** 2) + (a ** 2) - (b ** 2)) / (2 * a * c)
+# cos_C = ((a ** 2) + (b ** 2) - (c ** 2)) / (2 * a * b)
+# A_d = math.degrees(math.acos(cos_A))
+# B_d = math.degrees(math.acos(cos_B))
+# C_d = math.degrees(math.acos(cos_C))
+
+# print(f'The 3 angles are :\n A : {A_d} \n B : {B_d} \n C : {C_d}')
+# ----------------------------------------------------------
+
+# -------------moment and force ------------------
+# || Usage: enter f1 and d1, select what answers do you need. ||
+# ------------------------------------------------
+# print ("Usage: enter f1 and d1, select what answers do you need. ")
+# f_1 = float(input("Enter the force 1: "))
+# d_1 = float(input("Enter the distance between force 1 and pivoted point: "))
+# bool = input("You need to find the force 2 or the distance? f/d :")
+# if bool == "f":
+#     d_2 = float(input("What is the distance of force 2 to the pivoted point: "))
+#     ans = (f_1 * d_1) / d_2
+#     print(f'The force 2 is : {ans}')
+# elif bool == "d":
+#     f_2 = float(input("What is the force 2: "))
+#     ans = (f_1 * d_1) / f_2
+#     print(f'The distance between the point and force 2 is : {ans}')
+# ------------------------------------------------------------------------
+
