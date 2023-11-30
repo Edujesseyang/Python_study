01dee3c9b18eeb50da49114eccfe2fe83b494cd2
Edujesseyang <edujesseyang@gmail.com>
2023-11-29 18:41:59 -0800

    as

--- practice.py
+++ practice.py
@@ -1038,55 +1038,89 @@ from collections import namedtuple
 # first_name, last_name, id, courses. Classes should be a dictionary that maps a course id to a list containing course name, 
 # grade as percentage float, and grade as string letter (i.e. B+). Percentage float should be rounded to 2 decimal places.
 # Solution:
-class Student:
-    def __init__(self, first_name,last_name, id):
-        self.first_name = first_name
-        self.last_name = last_name
-        self.id = id
-        self.courses = []
-        self.grade = ""
-        self.gpa = 0.0
-        self.info = {}
-    def get_info(self):
-        self.info = {
-        "First name" : self.first_name,
-        "Last name" : self.last_name,
-        "ID" : self.id,
-        "Course" : self.courses,
-        "Grade" : self.grade,
-        "GPA" : self.gpa
-        }
-        return self.info
-    def print_info(self):
-        '''No return'''
-        print(f'{self.last_name}, {self.first_name}:\n ID:   {self.id}\n Course:   {self.courses}\n   Grade:{self.grade}\n   GPA: {self.gpa}')
-    def add_courses(self):
+# class Student:
+#     def __init__(self, first_name,last_name, id):
+#         self.first_name = first_name
+#         self.last_name = last_name
+#         self.id = id
+#         self.courses = []
+#         self.grade = ""
+#         self.gpa = 0.0
+#         self.info = {}
+#     def get_info(self):
+#         self.info = {
+#         "First name" : self.first_name,
+#         "Last name" : self.last_name,
+#         "ID" : self.id,
+#         "Course" : self.courses,
+#         "Grade" : self.grade,
+#         "GPA" : self.gpa
+#         }
+#         return self.info
+#     def print_info(self):
+#         '''No return'''
+#         print(f'{self.last_name}, {self.first_name}:\n ID:   {self.id}\n Course:   {self.courses}\n   Grade:{self.grade}\n   GPA: {self.gpa}')
+#     def add_courses(self):
         
-        '''No input, No return'''
-        self.input = (input("Enter your courses: "))
-        while self.input != "":
-            self.input = (input("Enter your courses: "))
-            self.courses.append(self.input)
-        self.courses.pop()
-        self.grade = input("Enter your grade: ")
-        valid = False
-        while valid == False:
-            try:            
-                self.gpa = float(input("Enter your GPA: "))
-                valid = True
-            except ValueError:
-                print("Invalid GPA.")
+#         '''No input, No return'''
+#         self.input = (input("Enter your courses: "))
+#         while self.input != "":
+#             self.input = (input("Enter your courses: "))
+#             self.courses.append(self.input)
+#         self.courses.pop()
+#         self.grade = input("Enter your grade: ")
+#         valid = False
+#         while valid == False:
+#             try:            
+#                 self.gpa = float(input("Enter your GPA: "))
+#                 valid = True
+#             except ValueError:
+#                 print("Invalid GPA.")
         
-'''Create student_1'''
-student_1 = Student("Jesse", "Yang", 20545215)
-student_1.add_courses()
+# '''Create student_1'''
+# student_1 = Student("Jesse", "Yang", 20545215)
+# student_1.add_courses()
 
-'''TEST'''
-print(student_1.get_info())
-student_1.print_info()
+# '''TEST'''
+# print(student_1.get_info())
+# student_1.print_info()
 
 # ===================================================
 
 # ==================================================
+# 1. Prompt the user to write a few sentences about future academic interests. 
+# Split the string into individual words and
+# write them out line by line into a file called words.txt.
+#
+# 2. Read in the words from the text file line by line and calculate the average word length. Then, print it to screen.
+# -------------------
+# solution:
+user_input = input("Enter a few lines sentences about future academic: ")
+input_words = user_input.split(" ")
+print(input_words)
 
-        
+txt_file = open("text.txt", "w")
+
+for word in input_words:
+    txt_file.write(word)
+    txt_file.write("\n")
+txt_file.close()
+
+file_open = open("text.txt", "r")
+txt_read = file_open.readlines()
+
+word_count = 0
+total_length = 0
+for word in txt_read:
+    total_length += len(word[:-1])
+    word_count += 1
+avg_length = total_length / word_count
+
+print(f'Total length is {total_length} \nWord count is {word_count}')
+print(f'The avg length of those words is {round(avg_length, 1)}')
+file_open.close()
+
+file_open = open("text.txt", "r")
+content = file_open.read()
+print(content)
+# ==========================================
