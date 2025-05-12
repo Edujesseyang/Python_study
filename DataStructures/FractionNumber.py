6781ccf72603f44d9a35918e1ec3866f512e338a
Jesse <142279444+Edujesseyang@users.noreply.github.com>
2025-05-11 21:02:10 -0700

    add

---
+++ DataStructures/FractionNumber.py
@@ -0,0 +1,82 @@
+import math
+from warnings import catch_warnings
+
+
+class FractionNum:
+    def __init__(self, numerator, denominator):
+        if denominator == 0:
+            raise ValueError("Denominator cannot be zero")
+        self.numerator = numerator
+        self.denominator = denominator
+
+    def add(self, other):
+        other = FractionNum.to_fraction(other)
+
+        new_numerator = self.numerator * other.denominator + other.numerator * self.denominator
+        new_denominator = self.denominator * other.denominator
+
+        gcd = math.gcd(new_numerator, new_denominator)
+        return FractionNum(new_numerator // gcd, new_denominator // gcd)
+
+    def substra(self, other):
+        other = FractionNum.to_fraction(other)
+
+        new_numerator = self.numerator * other.denominator - other.numerator * self.denominator
+        new_denominator = self.denominator * other.denominator
+
+        gcd = math.gcd(new_numerator, new_denominator)
+        return FractionNum(new_numerator // gcd, new_denominator // gcd)
+
+    def multiple(self, other):
+        other = FractionNum.to_fraction(other)
+
+        new_numerator = self.numerator * other.numerator
+        new_denominator = self.denominator * other.denominator
+
+        gcd = math.gcd(new_numerator, new_denominator)
+        return FractionNum(new_numerator // gcd, new_denominator // gcd)
+
+    def divide(self, other):
+        other = FractionNum.to_fraction(other)
+
+        new_numerator = self.numerator * other.denominator
+        new_denominator = self.denominator * other.numerator
+
+        gcd = math.gcd(new_numerator, new_denominator)
+        return FractionNum(new_numerator // gcd, new_denominator // gcd)
+
+    def simplify(self):
+        greatest_factor = math.gcd(self.denominator, self.numerator)  # Math.gcd() 为python自带取n数的最大公约数
+        self.numerator = self.numerator // greatest_factor  # // 为整除操作
+        self.denominator = self.denominator // greatest_factor
+
+    def __str__(self):
+        return f"{self.numerator} / {self.denominator}"
+
+    @staticmethod
+    def to_fraction(num):
+        if isinstance(num, FractionNum):
+            return num
+        elif isinstance(num, int):
+            denominator = 1
+        elif isinstance(num, float):
+            s = str(num)
+            if '.' in s:
+                after_decimal = len(s.split('.')[1])
+                num = int(num * (10 ** after_decimal))
+                denominator = 10 ** after_decimal
+        return FractionNum(num, denominator)
+
+
+f1 = FractionNum(1, 3)
+f2 = FractionNum(4, 7)
+print(f1.add(f2))
+print(f1.substra(f2))
+print(f1.multiple(f2))
+print(f1.divide(f2))
+print(f1.add(3).multiple(5).substra(12))
+print(f1.add(3).multiple(5).substra(12).divide(f2))
+try:
+    print(f1.add(3).multiple(5).substra(12).divide(0))
+except ValueError:
+    print("Can divide zero")
